6664db57619f06b3fd2420bd4efedaa58d003197
Alexandre Percebois <alex.percebois@gmail.com>
2018-11-09 16:35:14 +0100

    Prototype affichage des tâches

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,6 +1,9 @@
+import { FormGroup, FormControl } from '@angular/forms';
+import { EtatsService } from './../etats.service';
 import { Component, OnInit } from '@angular/core';
 import { ElectronService } from 'ngx-electron';
 import { Users } from '../users.service';
+import { Etat } from '../etats.service';
 
 @Component({
   selector: 'app-main',
@@ -8,15 +11,24 @@ import { Users } from '../users.service';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
+  public etats: Array<Etat>;
   public currentUser: Users;
 
-  constructor(private elec: ElectronService) { }
+  public form = new FormGroup({
+    keywork: new FormControl('')
+  });
+
+  constructor(
+    private elec: ElectronService,
+    public etatService: EtatsService
+  ) {}
 
   ngOnInit() {
     this.currentUser = JSON.parse(localStorage.getItem('user'));
     this.elec.remote.getCurrentWindow().maximize();
     this.elec.remote.getCurrentWindow().show();
+    this.etatService.GetAll().then(res => {
+      this.etats = res;
+    });
   }
-
 }
